6e9ed74bb15e175b2d32c39a64be3ab38dad01e9
Khanh Nguyen <nmkhanhbk@gmail.com>
2022-10-12 21:55:30 +0700

    Add UpdateUser api to partially update user

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -50,4 +51,23 @@ func TestGetUser(t *testing.T) {
 	assert.Equal(t, user1.Email, user2.Email)
 	assert.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
 	assert.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUser_OnlyFullName(t *testing.T) {
+	oldUser := createRandomUser(t)
+	newFullName := util.RandomOwner()
+
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+	})
+	assert.NoError(t, err)
+	assert.NotEqual(t, oldUser.FullName, updatedUser.FullName)
+
+	assert.Equal(t, newFullName, updatedUser.FullName)
+	assert.Equal(t, oldUser.Email, updatedUser.Email)
+	assert.Equal(t, oldUser.HashedPassword, updatedUser.HashedPassword)
 }
